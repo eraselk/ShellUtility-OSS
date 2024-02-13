5d9b0880b9423acce9e498448444271f1178d115
eraselk <141639047+eraselk@users.noreply.github.com>
2024-02-13 20:24:10 +0800

    Import ShellUtility-r024125 Build

--- .lib/libenc.sh
+++ .lib/libenc.sh
@@ -2,34 +2,46 @@ dir1=/data/data/com.termux/files/usr/bin
 dir2=/data/data/com.termux/files/home
 SP=/storage/emulated/0/ShellUtility
 
-function cl() {
-clear
-}
-
-function pr() {
-echo "$1"
-}
-
-function inpt() {
-read -p "$1" $2
-}
-
-function sl() {
-sleep $1 
-}
-
-function install_pkg() {
-pkg update -y ; pkg upgrade -y ; pkg install shc -y ; pkg install binutils -y ; pkg install nano -y
-# detect if installed libpolly
-if [[ -e "/data/data/com.termux/files/usr/var/lib/dpkg/info/libpolly.md5sums" ]]; then
+cl() { clear; }
+pr() { echo "$1"; }
+inpt() { read -p "$1" "$2"; }
+sl() { sleep $1; }
+install_pkg() {
+pkg update -y ; pkg upgrade -y ; pkg install shc -y ; pkg install binutils -y ; pkg install nano -y; pkg install git -y; pkg install libandroid-wordexp -y; pkg install busybox; busybox --install $PREFIX/bin
+# detect if libpolly installed
+if [[ $(pkg list-installed) == *libpolly* ]]; then
 pkg clean ; pkg uninstall libpolly -y
 fi
 }
 
-function end() {
-exit 0
+end() { exit 0; }
+abort() { exit 1; }
+open_telegram() { am start -a android.intent.action.VIEW -d https://t.me/gacorpkjrt; }
+update_packages() {
+    clear
+    sleep 2s
+    echo
+    echo "[*] Please wait..."
+    echo
+    pkg update -y ; pkg upgrade -y ; pkg install shc -y ; pkg install binutils -y ; pkg install nano -y; pkg install git -y; pkg install libandroid-wordexp -y; pkg install busybox -y; busybox --install $PREFIX/bin
+    echo 
+    echo "[*] Done"
+    sleep 3s
+    menu
 }
-
-function abort() {
-exit 1
-} 
+uninstall_script() {
+cl
+sl 2s
+pr "[*] Uninstalling script..."
+sl 3s
+rm -f ${dir1}/enc
+rm -f ${dir1}/dec
+rm -f ${dir2}/.build_date
+rm -rf ${SP}
+$HOME/.ssc/uninstall.sh
+pr "[*] Done!, See you:)"
+end
+}
+edit_decrypter_script() { 
+nano ${dir1}/dec; menu
+}
