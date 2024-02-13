d86522ca689033bbe30d3d669c2bb03d5ed94154
eraselk <141639047+eraselk@users.noreply.github.com>
2024-02-13 20:25:08 +0800

    Import ShellUtility-r024125 Build

--- setup.sh
+++ setup.sh
@@ -5,18 +5,26 @@ pr "[*] Installing Packages..."
 pr
 install_pkg
 pr
-pr "Q: Are you Root user? [y/n]"
+pr "[*] Cloning SSC Repository..."
+if [[ -d "$HOME/.ssc" ]]; then
+$HOME/.ssc/uninstall.sh > /dev/null
+fi
+git clone --depth=1 https://github.com/eraselk/ssc-android ssc
+rm -rf $HOME/ssc/examples
+chmod 777 $HOME/ssc/*; $HOME/ssc/inject.sh > /dev/null
+pr
+pr "Q: Are you a root user? [y/n]"
 inpt ": " akar
 pr ""
 if [[ $akar == "y" ]]; then
    pkg install tsu -y > /dev/null
-   rm ${SP}/rawnon ${SP}/decnon
-   mv ${SP}/rawroot ${SP}/raw
-   mv ${SP}/decroot ${SP}/dec
+   rm -f ${SP}/rawnon ${SP}/decnon
+   mv -f ${SP}/rawroot ${SP}/enc
+   mv -f ${SP}/decroot ${SP}/dec
 elif [[ $akar == "n" ]]; then
-    rm ${SP}/rawroot ${SP}/decroot
-    mv ${SP}/rawnon ${SP}/raw
-    mv ${SP}/decnon ${SP}/dec
+    rm -f ${SP}/rawroot ${SP}/decroot
+    mv -f ${SP}/rawnon ${SP}/enc
+    mv -f ${SP}/decnon ${SP}/dec
 else
   pr "Blud, use your brain."
   exit 1
@@ -25,16 +33,18 @@ pr "[*] Make output directory"
 mkdir $SP/output
 pr "[*] Building Script.."
 if [[ -e "${dir2}/.build_date" ]]; then
-rm ${dir2}/.build_date
-else
-echo "$(date)" >> ${dir2}/.build_date
-shc -r -f ${SP}/raw -o ${SP}/enc
-cp ${SP}/enc ${dir1} 
-chmod 777 ${dir1}/enc
-cp ${SP}/dec ${dir1} 
-chmod 777 ${dir1}/dec
+rm -f ${dir2}/.build_date
 fi
+if [[ -e "${dir1}/enc" ]]; then 
+rm -f ${dir1}/enc
+fi
+if [[ -e "${dir1}/dec" ]]; then
+rm -f ${dir1}/dec
+fi
+echo "$(date)" > ${dir2}/.build_date
+mv -f ${SP}/enc ${dir1}; chmod 777 ${dir1}/enc
+mv -f ${SP}/dec ${dir1}; chmod 777 ${dir1}/dec
 pr "[*] Cleaning Up"
-rm ${SP}/setup.sh ; rm ${SP}/raw ; rm ${SP}/raw.x.c ; rm ${SP}/enc ; rm ${SP}/dec
+rm -f ${SP}/setup.sh; rm -f ${SP}/raw; rm -f ${SP}/raw.x.c
 pr "[*] Setup Done!."
 end
